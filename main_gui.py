46455a67fa55de849993b55ce0af2f20c3bc202c
yana <yanachagalyan@gmail.com>
2024-09-23 19:06:39 +0200

    last touches

--- main_gui.py
+++ main_gui.py
@@ -5,21 +5,18 @@ import customtkinter as ctk
 from tkinter import *
 import tkinter
 
-# simplified code (compared to the main.py) :D, a bit buggy but does the job!
+# simplified code (compared to the main.py) :D, a bit buggy because I didn't put it into a class, but does the job!
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
 app = ctk.CTk()
 
-app.geometry = ("800x400")
-app.grid_columnconfigure((0, 1), weight=1)
-
 while True:
     df = pd.read_csv('ClassicHit.csv')        
 
     # QUESTION 1 - MOOD
-    question_1 = ctk.CTkInputDialog(text='''
+    question_1 = ctk.CTkInputDialog(title="Song Selector", text='''
     Hi, user! Welcome to the Song Selector - you'll get a tailored song recommendation after answering a few simple questions!
     Your answer options are indicated by [1] or [2] in the question - please make sure to type exactly what you see.
     Example: "Do you want a song to be [1]shorter or [2]longer?: "
@@ -31,11 +28,11 @@ while True:
     answer_1 = question_1.get_input()
 
     if answer_1.lower() == 'happy':
-        question_2 = ctk.CTkInputDialog(text='''\nSomeone\'s in a good mood :D Okay, next question:\n
+        question_2 = ctk.CTkInputDialog(title="Song Selector", text='''\nSomeone\'s in a good mood :D Okay, next question:\n
 Do you want a [1]longer or a [2]shorter one?''', font=("Arial", 12),button_fg_color='#1a1a1a',button_text_color='#1a1a1a')
         df = df[df['Mode'] == 1]  
     elif answer_1.lower() == 'sad':
-        question_2 = ctk.CTkInputDialog(text='''\nA bad day? :( It\'s okay, let\'s go to the next question:\n
+        question_2 = ctk.CTkInputDialog(title="Song Selector", text='''\nA bad day? :( It\'s okay, let\'s go to the next question:\n
 Do you want a [1]longer or a [2]shorter one?''', font=("Arial", 12),button_fg_color='#1a1a1a',button_text_color='#1a1a1a')
         df = df[df['Mode'] == 0]      
 
@@ -49,7 +46,7 @@ Do you want a [1]longer or a [2]shorter one?''', font=("Arial", 12),button_fg_co
     
     # QUESTION 3 - GENRE
     count_songs_per_genre = df.groupby('Genre').size().to_string()
-    question_3 = ctk.CTkInputDialog(text=f'''
+    question_3 = ctk.CTkInputDialog(title="Song Selector", text=f'''
 \nGotcha! I\'ll select a good one for you :D\n
 Okay, now which genre do you prefer? Here's how many songs is left per genre:\n
 {count_songs_per_genre}\n
@@ -64,7 +61,7 @@ What do you choose? ''', font=("Arial", 12),button_fg_color='#1a1a1a',button_tex
     count_songs_20_century = df[(df['Year'] > 1900) & (df['Year'] < 2000)].shape[0]
     count_songs_21_century = df[df['Year'] >= 2000].shape[0]
 
-    question_4 = ctk.CTkInputDialog(text=f'''\nOooh {answer_3.lower()} is my favourite genre as well! Aaand onto the last question!\n
+    question_4 = ctk.CTkInputDialog(title="Song Selector", text=f'''\nOooh {answer_3.lower()} is my favourite genre as well! Aaand onto the last question!\n
 We have lots of songs here, old and new - which century would you want to choose? 
 P.S. If you want a random song, type 0
 P.P.S. Here's how many songs are left per century:
@@ -90,7 +87,7 @@ What do you choose? ''', font=("Arial", 12),button_fg_color='#1a1a1a',button_tex
     artist = filtered_result['Artist'].values[0]
     year = filtered_result['Year'].values[0]
                 
-    question_5 = ctk.CTkInputDialog(text=f'''\nThanks for the answers! Based on your choices, here\'s a random song to your liking: 
+    question_5 = ctk.CTkInputDialog(title="Song Selector", text=f'''\nThanks for the answers! Based on your choices, here\'s a random song to your liking: 
 It's '{track}' by {artist}, recorded in {year}\n
 Do you want to listen to the song you got? (yes/no)''', font=("Arial", 12),button_fg_color='#1a1a1a',button_text_color='#1a1a1a')
     answer_5 = question_5.get_input()
@@ -99,12 +96,6 @@ Do you want to listen to the song you got? (yes/no)''', font=("Arial", 12),butto
         break
     elif answer_5 == 'yes':
         spc.search_track(spc.token, track, artist)
-        try_again =  ctk.CTkInputDialog(text="\nWanna try again? (yes/no) ", font=("Arial", 12),button_fg_color='#1a1a1a',button_text_color='#1a1a1a')
-        try_again_answer = try_again.get_input()
-        if try_again_answer == 'yes':
-            continue
-        else:            
-            break
 
 
     app.mainloop()
